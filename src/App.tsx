d344107cfb48d9b82a377158db53f1bf5bc70896
ingridflack <ingrid_flack@hotmail.com>
2024-11-06 14:07:11 -0300

    Add modal logic and update drag point logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import { CRS, GeoJSON, Icon, LatLng, LeafletMouseEvent, PolyUtil } from "leaflet";
+import { CRS, DragEndEvent, GeoJSON, Icon, LatLng, LeafletMouseEvent, PolyUtil } from "leaflet";
 import { MapContainer, Marker, GeoJSON as GeoJsonComponent, TileLayer, useMapEvents, ZoomControl } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 import fieldGeoJson from "./assets/field.json";
 import ButtonList from "./components/ButtonList";
 import defaultPinIcon from "./assets/icons/default-pin.svg";
 import selectedPinIcon from "./assets/icons/selected-pin.svg";
-import { Point } from "./interfaces/common";
+import { DeleteModalType, Point } from "./interfaces/common";
 import PointsList from "./components/PointsList";
+import GlobalStyle from "./globalStyle";
+import ModalConfirm from "./components/ModalConfirm";
 
 const defaultPin = new Icon({
   iconUrl: defaultPinIcon,
@@ -27,14 +29,26 @@ interface MapPointsProps {
   onMarkerClick: (id: string) => void;
 }
 
-const MapPoints = ({ points, selectedPoint, onMapClick, onMarkerClick }: MapPointsProps) => {
-  const map = useMapEvents({
+interface DeleteModal {
+  show: boolean;
+  id?: string | null;
+  type?: DeleteModalType;
+}
+
+const MapPoints = ({ points, selectedPoint, onPointDragEnd, onMapClick, onMarkerClick }: MapPointsProps) => {
+  useMapEvents({
     click: (event: LeafletMouseEvent) => {
       onMapClick(event.latlng);
     }
   });
 
-  // @TODO: Update the selected point position after dragging it.
+  const handleClick = (id: string) => () => {
+    onMarkerClick(id);
+  }
+
+  const handleDragEnd = (id: string) => (event: DragEndEvent) => {
+    onPointDragEnd(id, event.target.getLatLng());
+  }
 
   return points.map(({ id, position }) => {
     const isSelected = selectedPoint === id;
@@ -46,22 +60,26 @@ const MapPoints = ({ points, selectedPoint, onMapClick, onMarkerClick }: MapPoin
         position={position}
         draggable={isSelected}
         eventHandlers={{
-          click: () => {
-            onMarkerClick(id);
-          }
+          click: handleClick(id),
+          dragend: handleDragEnd(id),
         }}
       />
     )
   })
 }
 
+const getFieldCenter = () => {
+  return PolyUtil.polygonCenter(fieldGeoJson.features[0].geometry.coordinates[0].map(([lat, lon]) => [lon, lat]), CRS.EPSG3857);
+}
+
 function App() {
   const [points, setPoints] = useState<Point[]>([]);
+  const [deleteModal, setDeleteModal] = useState<DeleteModal>({ show: false });
   const [selectedPoint, setSelectPoint] = useState<string | null>(null);
 
   const handleAddPoint = (position?: LatLng) => {
     if (!position) {
-      position = PolyUtil.polygonCenter(fieldGeoJson.features[0].geometry.coordinates[0].map(([lat, lon]) => [lon, lat]), CRS.EPSG3857);
+      position = getFieldCenter();
     }
 
     setPoints((points) => [...points, {
@@ -72,6 +90,25 @@ function App() {
 
     setSelectPoint(null);
   }
+
+  const handleOpenDeletePointModal = () => {
+    setDeleteModal({
+      show: true,
+      type: 'delete',
+      id: selectedPoint,
+    });
+  };
+
+  const handleOpenDeleteAllModal = () => {
+    setDeleteModal({
+      show: true,
+      type: 'delete-all',
+    });
+  };
+
+  const handleCloseModal = () => {
+    setDeleteModal({ show: false });
+  };
 
   const handleDeletePoint = (id: string) => {
     setPoints((points) => points.filter(point => point.id !== id));
@@ -87,28 +124,51 @@ function App() {
     setSelectPoint(id);
   }
 
+  const handleDeleteModal = () => {
+    if (!deleteModal.id) {
+      handleDeleteAllPoints();
+    } else {
+      handleDeletePoint(deleteModal.id);
+    }
+
+    setDeleteModal({ show: false });
+  }
+
+  const handlePointDragEnd = (id: string, position: LatLng) => {
+    setPoints((points) => points.map(point => {
+      if (point.id === id) {
+        return {
+          ...point,
+          position,
+        }
+      }
+
+      return point;
+    }));
+  }
+
   return (
     <div>
+      <GlobalStyle />
+
       <h1>Gestão de pontos no mapa</h1>
 
       <div style={{ position: "relative" }}>
-        <MapContainer center={fieldGeoJson.features[0].geometry.coordinates[0].map(([lat, lon]) => [lon, lat])[0]} zoom={13} scrollWheelZoom={false} style={{ height: "600px" }} zoomControl={false}>
+        <MapContainer center={getFieldCenter()} zoom={15} scrollWheelZoom style={{ height: "600px" }} zoomControl={false}>
           <TileLayer
             attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
           />
-
-          <MapPoints points={points} selectedPoint={selectedPoint} onMapClick={handleAddPoint} onMarkerClick={handleSelectPoint} />
-
+          <MapPoints points={points} selectedPoint={selectedPoint} onPointDragEnd={handlePointDragEnd} onMapClick={handleAddPoint} onMarkerClick={handleSelectPoint} />
           <GeoJsonComponent data={fieldGeoJson} style={({ properties }) => properties} />
-
           <ZoomControl position="topright" />
         </MapContainer>
 
-        <ButtonList points={points} selectedPoint={selectedPoint} onAdd={handleAddPoint} onDelete={handleDeletePoint} onDeleteAll={handleDeleteAllPoints} />
-
-        <PointsList points={points} selectedPoint={selectedPoint} />
+        <PointsList points={points} selectedPoint={selectedPoint} onSelectPoint={handleSelectPoint} />
+        <ButtonList points={points} selectedPoint={selectedPoint} onAdd={handleAddPoint} onDelete={handleOpenDeletePointModal} onDeleteAll={handleOpenDeleteAllModal} />
       </div >
+
+      <ModalConfirm isOpen={deleteModal.show} onConfirm={handleDeleteModal} onClose={handleCloseModal} type={deleteModal.type} />
     </div >
   )
 }
